e1b756d3a6a2dd81e215cc0f652c667460bd63d6
Tiago <ottiagos@gmail.com>
2020-06-04 23:14:39 -0300

    Add pedido - Sagas

--- redux/pedido/pedido-saga.js
+++ redux/pedido/pedido-saga.js
@@ -1,12 +1,38 @@
 import { delay, takeEvery , put, all, call } from 'redux-saga/effects';
 import PedidoActionTypes from './pedido.types';
 import { pedidoAddSuccess} from './pedido.actions';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export function* addPedido(action) {
-    try{
+    const { payload } = action;
 
+    try{
+        let ar = [];
+
+        AsyncStorage.getItem('pedidos').then(
+            pedidos => {
+            //   let id = this.lastIdPlusOne(turmas);
+    
+              if(pedidos){
+                console.log(pedidos)
+                ar = JSON.parse(pedidos);
+                p = payload; 
+                ar.push(p);
+                AsyncStorage.setItem('pedidos', JSON.stringify(ar));
+    
+              }else{
+                p = payload;
+                console.log("sem pedidos ainda")
+                ar.push(p);
+                AsyncStorage.setItem('pedidos', JSON.stringify(ar));
+              }
+              
+            }
+          )
+        console.log(payload)
+        yield put(pedidoAddSuccess(ar));
     }catch{
-        
+
     }
 }
 
